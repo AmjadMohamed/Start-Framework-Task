2de7aef6ff5b7ed312caf27fb513ec7dc862e3db
Amjad Mohamed <amjadmohamed55@gmail.com>
2025-08-08 06:39:27 +0300

    Fix image loading configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Start-Framework-Task/',
+  build: {
+    assetsDir: 'assets',
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg']
 })
